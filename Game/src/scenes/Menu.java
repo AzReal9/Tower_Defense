a2533d79b77c75f1b527fb1f412d71429a2eca63
AzReal9 <austin178c@gmail.com>
2024-02-15 09:27:21 -0500

    Fixed the old game sprite logic(Austin)
    I fixed the functionality, should be good now.

--- Game/src/scenes/Menu.java
+++ Game/src/scenes/Menu.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 public class Menu extends GameScene implements SceneMethods{
@@ -17,47 +16,46 @@ public class Menu extends GameScene implements SceneMethods{
     private BufferedImage img;
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
     private Random random;
-    private BufferedImage grass;
+    private BufferedImage Img;
     
     public Menu(Game game) {
         super(game);
         random = new Random();
-        importGrass();
-        //loadSprites();
+        importImg();
+        loadSprites();
     }
 
     @Override
     public void render(Graphics g) {
-                      for(int y = 0; y < 29; y++){
-            for(int x = 0; x < 29; x++){
-                g.drawImage(grass, x *32, y * 32, null);
+                      for(int y = 0; y < 20; y++){
+            for(int x = 0; x < 20; x++){
+                g.drawImage(sprites.get(getRndInt()), x *32, y * 32, null);
             } 
         }
     }
     
-        /*private void loadSprites() {
-        
-        for(int y = 0; y < 10; y++){
-            for (int x = 0; x < 10; x++){
-                
+    public void importImg(){
+       
+       InputStream is = getClass().getResourceAsStream("/Sprites/spriteatlas.png"); //Finds the Path for the images
+       try {       
+           img = ImageIO.read(is);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+   }
+    
+    private void loadSprites() {
+    
+        for(int y = 0; y < 10; y++) {
+            for(int x = 0; x < 10; x++) {
+                sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
             }
         }
-       
-    }*/
+    }
     
     private int getRndInt() {
         return random.nextInt(100);
     }
-    
-    public void importGrass(){
-       
-       InputStream is = getClass().getResourceAsStream("/Sprites/grass.png"); //Finds the Path for the images
-       try {       
-           grass = ImageIO.read(is);
-       } catch (IOException ex) {
-           Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
-       }
-
-   }
     
 }
